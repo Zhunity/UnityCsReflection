b4ef1bf0ca6be6438756e2ca93d6266232ba8feb
lihua <poinseting@163.com>
2020-11-09 23:36:07 +0800

    移除组件时机

---
+++ Editor/Mine/Extension/Component/RemoveComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SMFrame.Editor.Extension
+{
+
+
+	public static class RemoveComponent 
+	{
+		[MenuItem("CONTEXT/Image/Remove Component")]
+		public static void RemoveImage(MenuCommand menuCommand)
+		{
+			var img = menuCommand.context as Image;
+			var canvasRenderer = img.GetComponent<CanvasRenderer>();
+			UnityEngine.Object.DestroyImmediate(img);
+
+			UnityEngine.Object.DestroyImmediate(canvasRenderer);
+		}
+
+		[MenuItem("CONTEXT/Text/Remove Component")]
+		public static void RemoveText(MenuCommand menuCommand)
+		{
+			var txt = menuCommand.context as Text;
+			var canvasRenderer = txt.GetComponent<CanvasRenderer>();
+			UnityEngine.Object.DestroyImmediate(txt);
+
+			UnityEngine.Object.DestroyImmediate(canvasRenderer);
+		}
+	}
+}
