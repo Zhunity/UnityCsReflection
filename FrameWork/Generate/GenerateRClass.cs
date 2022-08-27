d16ca868ba1dd1a3a593b8ec928150409c33f807
lizhenhua.0769 <lizhenhua.0769@bytedance.com>
2022-08-27 23:34:23 +0800

    提取GenerateMethod

--- FrameWork/Generate/GenerateRClass.cs
+++ FrameWork/Generate/GenerateRClass.cs
@@ -26,22 +26,22 @@ namespace SMFrame.Editor.Refleaction
         }
     }
 
-    public class GenerateRClass
+    public partial class GenerateRClass
     {
         [MenuItem("Tools/fff")]
         static void G()
         {
-            // Generate<A>();
+            Generate<A>();
 
             A b = new A();
 
-            SMFrame.Editor.Refleaction.RA a = new SMFrame.Editor.Refleaction.RA();
-            a.SetInstance(b);
+            //SMFrame.Editor.Refleaction.RA a = new SMFrame.Editor.Refleaction.RA();
+            //a.SetInstance(b);
 
-            Debug.Log(a.N<int, float>(1, " ", 2f));
-            Debug.Log(a.N<float>());
+            //Debug.Log(a.N<int, float>(1, " ", 2f));
+            //Debug.Log(a.N<float>());
 
-            Debug.Log(a.N());
+            //Debug.Log(a.N());
         }
 
         public static void Generate(Type classType)
@@ -104,18 +104,7 @@ namespace SMFrame.Editor.Refleaction
                 //newStr += GenerateMemberNew(null, @event.Name, "Event");
             }
 
-            string methodInvoke = string.Empty;
-            var methods = classType.GetMethods(Class.flags);
-            foreach (var method in methods)
-            {
-                if(getSetHash.Contains(method))
-                {
-                    continue;
-                }
-                delcareStr += GenerateMethodDeclare(method);
-                newStr += GenerateMethodNew(method);
-                methodInvoke += GenerateMethodInvoke(method);
-            }
+            string methodInvoke = GenerateMethod(classType, getSetHash, ref delcareStr, ref newStr);
 
             var generateStr = $@"{nameSpaceStr}using SMFrame.Editor.Refleaction;
 using System;
@@ -189,132 +178,6 @@ namespace SMFrame.Editor.Refleaction.{classType.Namespace}
         {
             string propertyType = IsPrimitive(type) ? memberType : "R" + type.Name;
             return $"\t\t\t{name} = new {propertyType}(this, \"{name}\");\n";
-        }
-
-        private static string GenerateMethodDeclare(MethodInfo method)
-        {
-            string name = GetMethodName(method);
-
-            var generics = method.GetGenericArguments();
-            var parameters = method.GetParameters();
-
-            var noteStr = method.ReturnType.Name + " " + method.Name;
-            if(generics.Length > 0)
-            {
-                noteStr += "<";
-                for(int i = 0; i < generics.Length; i++)
-                {
-                    noteStr += generics[i].Name;
-                    if(i < generics.Length - 1)
-                    {
-                        noteStr += ", ";
-                    }
-                }
-                noteStr += ">";
-            }
-
-            noteStr += "(";
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                noteStr += parameters[i].ParameterType.Name + " " + parameters[i].Name;
-                if (i < parameters.Length - 1)
-                {
-                    noteStr += ", ";
-                }
-            }
-            noteStr += ")";
-            return GenerateDeclare("Method", name, noteStr);
-        }
-
-        private static string GenerateMethodNew(MethodInfo method)
-        {
-            string name = GetMethodName(method);
-
-            var generics = method.GetGenericArguments();
-            var parameters = method.GetParameters();
-            var paramStr = string.Empty;
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                paramStr += $", ReleactionUtils.GetType(\"{parameters[i].ParameterType.FullName}\")";
-            }
-
-            return $"\t\t\t{name} = new Method(this, \"{method.Name}\", {generics.Length}{paramStr});\n";
-        }
-
-        private static string GenerateMethodInvoke(MethodInfo method)
-        {
-            string name = GetMethodName(method);
-
-            string declareStr = $"public object {method.Name}";
-            var parameters = method.GetParameters();
-
-            var paramStr = string.Empty;
-            var generics = method.GetGenericArguments();
-            if (generics.Length > 0)
-            {
-                declareStr += "<";
-                paramStr = "new Type[] { ";
-                for (int i = 0; i < generics.Length; i++)
-                {
-                    declareStr += generics[i].Name;
-                    paramStr += $"typeof({generics[i].Name})";
-                    if (i < generics.Length - 1)
-                    {
-                        declareStr += ", ";
-                        paramStr += ", ";
-                    }
-                }
-                declareStr += ">";
-                paramStr += " }";
-
-                if(parameters.Length > 0)
-                {
-                    paramStr += ", ";
-                }
-            }
-
-            
-            declareStr += "(";
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                if(parameters[i].ParameterType.IsNotPublic)
-                {
-                    return string.Empty;
-                }
-                declareStr += parameters[i].ParameterType.FullName + " " + parameters[i].Name;
-                paramStr += parameters[i].Name;
-                if (i < parameters.Length - 1)
-                {
-                    declareStr += ", ";
-                    paramStr += ", ";
-                }
-            }
-            declareStr += ")";
-
-            var result = $@"
-        {declareStr}
-        {{
-            return {name}.Invoke({paramStr});
-        }}
-";
-            return result;
-        }
-
-        static private string GetMethodName(MethodInfo method)
-        {
-            var generics = method.GetGenericArguments();
-            string paramStr = "R" + method.Name;
-            foreach (var generic in generics)
-            {
-                paramStr += "_G" + generic.Name;
-            }
-
-            var parameters = method.GetParameters();
-            foreach (var parameter in parameters)
-            {
-                paramStr += "_" + parameter.ParameterType.Name;
-            }
-            return paramStr;
         }
 
 
---
+++ FrameWork/Generate/GenerateRMethod.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace SMFrame.Editor.Refleaction
+{
+    public partial class GenerateRClass
+    {
+        private static string GenerateMethod(Type classType, HashSet<MethodInfo> getSetHash, ref string delcareStr,  ref string newStr)
+        {
+            string methodInvoke = string.Empty;
+            var methods = classType.GetMethods(Class.flags);
+            foreach (var method in methods)
+            {
+                if (getSetHash.Contains(method))
+                {
+                    continue;
+                }
+                delcareStr += GenerateMethodDeclare(method);
+                newStr += GenerateMethodNew(method);
+                methodInvoke += GenerateMethodInvoke(method);
+            }
+
+            return methodInvoke;
+        }
+
+        private static string GenerateMethodDeclare(MethodInfo method)
+        {
+            string name = GetMethodName(method);
+
+            var generics = method.GetGenericArguments();
+            var parameters = method.GetParameters();
+
+            var noteStr = method.ReturnType.Name + " " + method.Name;
+            if (generics.Length > 0)
+            {
+                noteStr += "<";
+                for (int i = 0; i < generics.Length; i++)
+                {
+                    noteStr += generics[i].Name;
+                    if (i < generics.Length - 1)
+                    {
+                        noteStr += ", ";
+                    }
+                }
+                noteStr += ">";
+            }
+
+            noteStr += "(";
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                noteStr += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                if (i < parameters.Length - 1)
+                {
+                    noteStr += ", ";
+                }
+            }
+            noteStr += ")";
+            return GenerateDeclare("Method", name, noteStr);
+        }
+
+        private static string GenerateMethodNew(MethodInfo method)
+        {
+            string name = GetMethodName(method);
+
+            var generics = method.GetGenericArguments();
+            var parameters = method.GetParameters();
+            var paramStr = string.Empty;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                paramStr += $", ReleactionUtils.GetType(\"{parameters[i].ParameterType.FullName}\")";
+            }
+
+            return $"\t\t\t{name} = new Method(this, \"{method.Name}\", {generics.Length}{paramStr});\n";
+        }
+
+        private static string GenerateMethodInvoke(MethodInfo method)
+        {
+            string name = GetMethodName(method);
+
+            string declareStr = $"public object {method.Name}";
+            var parameters = method.GetParameters();
+
+            var paramStr = string.Empty;
+            var generics = method.GetGenericArguments();
+            if (generics.Length > 0)
+            {
+                declareStr += "<";
+                paramStr = "new Type[] { ";
+                for (int i = 0; i < generics.Length; i++)
+                {
+                    declareStr += generics[i].Name;
+                    paramStr += $"typeof({generics[i].Name})";
+                    if (i < generics.Length - 1)
+                    {
+                        declareStr += ", ";
+                        paramStr += ", ";
+                    }
+                }
+                declareStr += ">";
+                paramStr += " }";
+
+                if (parameters.Length > 0)
+                {
+                    paramStr += ", ";
+                }
+            }
+
+
+            declareStr += "(";
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                if (parameters[i].ParameterType.IsNotPublic)
+                {
+                    return string.Empty;
+                }
+                declareStr += parameters[i].ParameterType.FullName + " " + parameters[i].Name;
+                paramStr += parameters[i].Name;
+                if (i < parameters.Length - 1)
+                {
+                    declareStr += ", ";
+                    paramStr += ", ";
+                }
+            }
+            declareStr += ")";
+
+            var result = $@"
+        {declareStr}
+        {{
+            return {name}.Invoke({paramStr});
+        }}
+";
+            return result;
+        }
+
+        static private string GetMethodName(MethodInfo method)
+        {
+            var generics = method.GetGenericArguments();
+            string paramStr = "R" + method.Name;
+            foreach (var generic in generics)
+            {
+                paramStr += "_G" + generic.Name;
+            }
+
+            var parameters = method.GetParameters();
+            foreach (var parameter in parameters)
+            {
+                paramStr += "_" + parameter.ParameterType.Name;
+            }
+            return paramStr;
+        }
+    }
+}
