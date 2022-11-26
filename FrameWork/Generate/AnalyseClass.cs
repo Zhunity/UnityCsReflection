c3aa4b6cec22e22ba31913bf2db28a317252e9b0
lihua <poinseting@163.com>
2022-11-26 22:28:59 +0800

    提取通用的类转化逻辑

--- FrameWork/Generate/AnalyseClass.cs
+++ FrameWork/Generate/AnalyseClass.cs
@@ -9,8 +9,125 @@ using System.Text.RegularExpressions;
 
 namespace SMFrame.Editor.Refleaction
 {
+	public delegate string Translate(Type t);
+
+    public class TypeFormater
+    {
+        public string format;
+        public Translate translate;
+
+
+        public string Format(string elementStr)
+        {
+            return string.Format(format, elementStr);
+        }
+	}
+
+	public class TypeTranslater
+    {
+		public bool needNameSpace;
+		public TypeFormater Array;
+		public TypeFormater Pointer;
+        public TypeFormater GenericTypeDefinition;
+		public TypeFormater GenericType;
+        public TypeFormater GenericParameter;
+		public TypeFormater ByRef;
+        public Dictionary<Type, TypeFormater> type = new Dictionary<Type, TypeFormater>();
+	}
+
     public static class TypeToString
     {
+        public static string ToString(this Type type, TypeTranslater translater)
+        {
+			bool needNameSpace = translater.needNameSpace;
+
+			if (translater.type.TryGetValue(type, out var format))
+            {
+				return format.translate(type);
+            }
+			else if (type.IsArray)
+            {
+				var elementType = type.GetElementType();
+                return translater.Array.Format(elementType.ToString(translater));
+			}
+			else if (type.IsPointer)
+			{
+				var elementType = type.GetElementType();
+				return translater.Pointer.Format(elementType.ToString(translater));
+			}
+			else if (type.IsByRef)
+			{
+				var elementType = type.GetElementType();
+				return translater.ByRef.Format(elementType.ToString(translater));
+			}
+			// 这个要在IsGenericType前，因为IsGenericTypeDefinition也是IsGenericType
+			else if (type.IsGenericTypeDefinition)
+            {
+				var genericTypes = type.GetGenericArguments();
+				var genericDefine = type.GetGenericTypeDefinition();
+				string genericParamStr = string.Empty;
+				for (int i = 0; i < genericTypes.Length; i++)
+				{
+					if (i != genericTypes.Length - 1)
+					{
+						genericParamStr += ", ";
+					}
+				}
+
+				var defineName = Regex.Replace(genericDefine.Name, @"`\d+", translater.GenericTypeDefinition.Format(genericParamStr));
+				if (needNameSpace)
+				{
+					return genericDefine.Namespace + "." + defineName;
+				}
+				else
+				{
+					return defineName;
+				}
+			}
+			else if (type.IsGenericType)
+			{
+				// https://docs.microsoft.com/zh-cn/dotnet/framework/reflection-and-codedom/how-to-examine-and-instantiate-generic-types-with-reflection
+				var genericTypes = type.GetGenericArguments();
+				var genericDefine = type.GetGenericTypeDefinition();
+				string genericParamStr = string.Empty;
+				for (int i = 0; i < genericTypes.Length; i++)
+				{
+					var genericType = genericTypes[i];
+					var paramName = genericType.ToString(translater);
+					genericParamStr += paramName;
+					if (i != genericTypes.Length - 1)
+					{
+						genericParamStr += ", ";
+					}
+				}
+				//  typeof(System.Collections.Generic.Dictionary<, >).MakeGenericType(Type.MakeGenericMethodParameter(0), Type.MakeGenericMethodParameter(1))
+				var defineName = Regex.Replace(genericDefine.Name, @"`\d+", translater.GenericType.Format(genericParamStr));
+				if (needNameSpace)
+				{
+					return genericDefine.Namespace + "." + defineName;
+				}
+				else
+				{
+					return defineName;
+				}
+			}
+			else if(type.IsGenericParameter)
+			{
+				return type.Name;
+			}
+			else
+			{
+				if (needNameSpace)
+				{
+					return type.Namespace + "." + type.Name;
+				}
+				else
+				{
+					return type.Name;
+				}
+			}
+        }
+
         public static string ToFieldName(this Type type)
         {
             string name = String.Empty;
