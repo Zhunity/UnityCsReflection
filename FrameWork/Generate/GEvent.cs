2dd2f8c22ab53bdfeddf4d59f5e041db08fecfe5
lihua <poinseting@163.com>
2022-12-09 16:13:01 +0800

    genertate event

--- FrameWork/Generate/GEvent.cs
+++ FrameWork/Generate/GEvent.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 using UnityEngine;
 
 namespace SMFrame.Editor.Refleaction
@@ -19,5 +20,12 @@ namespace SMFrame.Editor.Refleaction
 		public override void GetRefTypes(HashSet<Type> refTypes)
 		{
 		}
-	}
+
+        public override void GetDeclareStr(StringBuilder sb)
+        {
+			string fieldType = "REvent";
+			var declareStr = GetDeclareStr(fieldType, @event.Name, @event.Name, @event.ToString());
+			sb.AppendLine(declareStr);
+		}
+    }
 }
