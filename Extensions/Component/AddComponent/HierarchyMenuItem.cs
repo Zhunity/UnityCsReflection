e4d5d44ab37a7d860028e079c491fafe439fcbc2
Zhunity <poinseting@163.com>
2023-03-13 21:34:07 +0800

    重写添加ui

---
+++ Extensions/Component/AddComponent/HierarchyMenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Hvak.Editor
+{
+	/// <summary>
+	/// Hierarchy的右键菜单
+	/// TODO 可参考UnityResourceReference  SceneHierarchy.AddCreateGameObjectItemsToMenu
+	/// </summary>
+	public class HierarchyMenuItem
+	{
+		[MenuItem("GameObject/UI/Image")]
+		static void CreatImage()
+		{
+			AddUI.InstanceImage();
+		}
+
+		[MenuItem("GameObject/UI/Text")]
+		static void CreatText()
+		{
+			AddUI.InstanceText();
+		}
+
+		[MenuItem("GameObject/UI/Text - TextMeshPro")]
+		static void CreatTMP()
+		{
+			AddUI.InstacneTMP();
+		}
+	}
+}
